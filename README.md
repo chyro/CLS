d1a127f5e410649e2179aa227c60dcf47c020f32
chyro <guilhem.malfre@gmail.com>
2017-05-12 06:00:12 +0000

    Upgraded all versions (Ubuntu, PHP, Phalcon, Mongo driver)

--- README.md
+++ README.md
@@ -4,3 +4,19 @@ Lifestyle
 Naive attempt at household management, with movie watchlists,
 shopping lists, holiday planning, etc.
 
+needs:
+- Phalcon
+- Mongo
+
+Tested with:
+- Ubuntu 16.04
+- Apache 2.4.18
+- PHP 7.0
+- Phalcon 3.1
+- php-mongodb 1.1.5
+- Phalcon Incubator branch 3.1
+
+TODO:
+- DB init script
+- URL mechanism for top- / sub-folder, single / multiple vhosts
+
--- public/index.php
+++ public/index.php
@@ -21,37 +21,38 @@ try {
 	//Register an autoloader
 	$loader = new \Phalcon\Loader();
 	$loader->registerNamespaces( [
-		"Watchlist\Controllers" => BASE_DIR . '/' . $config->application->controllersDir,
-		"Watchlist\Views" => BASE_DIR . '/' . $config->application->viewsDir,
-		"Watchlist\Models" => BASE_DIR . '/' . $config->application->modelsDir,
-		"Watchlist" => BASE_DIR . '/' . $config->application->appLibDir,
-		"Phalcore" => BASE_DIR . '/' . $config->application->librariesDir . "phalcore/"
+		'Watchlist\Controllers' => BASE_DIR . '/' . $config->application->controllersDir,
+		'Watchlist\Views' => BASE_DIR . '/' . $config->application->viewsDir,
+		'Watchlist\Models' => BASE_DIR . '/' . $config->application->modelsDir,
+		'Watchlist' => BASE_DIR . '/' . $config->application->appLibDir,
+		'Phalcore' => BASE_DIR . '/' . $config->application->librariesDir . 'phalcore/',
+		'Phalcon' => BASE_DIR . '/' . $config->application->vendorsDir . 'incubator/Library/Phalcon/',
 		] )->register();
 
 	//Create a DI
-	$config["di"] = new \Phalcon\DI\FactoryDefault();
+	$config['di'] = new \Phalcon\DI\FactoryDefault();
 
-	$config["di"]->set('session', function() {
+	$config['di']->set('session', function() {
 		$session = new \Phalcon\Session\Adapter\Files();
 		$session->start();
 		return $session;
 	});
 
 	//Setup the view component
-	$config["di"]->set('view', function() use ($config) {
+	$config['di']->set('view', function() use ($config) {
 		$view = new \Phalcon\Mvc\View();
 		$view->setViewsDir(BASE_DIR . '/' . $config->application->viewsDir);
 		return $view;
 	});
 
-	$config["di"]->set('dispatcher', function () {
+	$config['di']->set('dispatcher', function () {
 		$dispatcher = new \Phalcon\Mvc\Dispatcher();
 		$dispatcher->setDefaultNamespace('Watchlist\Controllers');
 		return $dispatcher;
 	});
 
 	//Setup the flash service
-	$config["di"]->set('flash', function () {
+	$config['di']->set('flash', function () {
 	    return new \Phalcon\Flash\Direct();
 	});
 
@@ -59,28 +60,38 @@ try {
 	include("../app/config/routes.php");
 
 	//DB Settings
-	$config["di"]->set('mongo', function() use ($config) {
-		$mongo = new MongoClient();
-		return $mongo->selectDB($config->database->name);
+	$config['di']->set('mongo', function() use ($config) {
+		/**
+		 * Migrating from Phalcon 2, PHP 5, MongoClient to Phalcon 3, PHP 7, MongoDB...
+		 * PHP 7 does not support MongoClient, Phalcon does not support MongoDB. Possible workarounds include:
+		 * - MongoDB / MongoClient compatibility class: https://github.com/alcaeus/mongo-php-adapter
+		 * - Phalcon incubator compatibility class: https://github.com/phalcon/incubator/tree/master/Library/Phalcon/Db/Adapter
+		 * Trying the last one. Hopefully the next version of Phalcon will incorporate those into the core
+		 * code in a compatible, seamless manner.
+		 */
+		//$mongo = new MongoClient();
+		$mongoClient = new \Phalcon\Db\Adapter\MongoDB\Client();
+		$db = $mongoClient->selectDatabase($config->database->name);
+		return $db;
 	}, true);
 
-	$config["di"]->set('collectionManager', function(){
+	$config['di']->set('collectionManager', function(){
 		return new \Phalcon\Mvc\Collection\Manager();
 	}, true);
 
-	$config["di"]->set('url', function(){
+	$config['di']->set('url', function(){
 		$url = new \Phalcon\Mvc\Url();
 		$url->setBaseUri('/');
 		return $url;
 	});
 
 	//Handle the request
-	$application = new \Phalcon\Mvc\Application($config["di"]);
+	$application = new \Phalcon\Mvc\Application($config['di']);
 
 	echo $application->handle()->getContent();
 
 } catch(\Phalcon\Exception $e) {
-	echo "\PhalconException: ", $e->getMessage();
-	echo "<pre>"; var_dump($e->getTrace()); echo "</pre>";
+	echo '\PhalconException: ', $e->getMessage();
+	echo '<pre>'; var_dump($e->getTrace()); echo '</pre>';
 }
 
